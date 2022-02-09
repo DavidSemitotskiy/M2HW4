0e059e879c4035b25ea1d0942a5a6956e174fcf8
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-02-09 13:45:44 +0200

    interface for DataProviders was added

---
+++ M2HW4/IDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW4
+{
+    public interface IDataProvider
+    {
+        public Animal[] GetData();
+    }
+}
