35d29cecf1da02a30e3b90a7e5d359487b7fb208
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-02-09 13:43:50 +0200

    interface for Sections was added

---
+++ M2HW4/ISection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW4
+{
+    public interface ISection
+    {
+        public Animal[] Animals { get; }
+        public void AddAnimal(Animal animal);
+        public string GetDescriptionSection();
+    }
+}
