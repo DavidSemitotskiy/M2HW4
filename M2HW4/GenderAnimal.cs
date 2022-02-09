ec4207814c5f0003b79dc0cb038df4c27d417473
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-02-09 13:32:28 +0200

    Gender and TypesOfAnimals were added

---
+++ M2HW4/GenderAnimal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW4
+{
+    public enum GenderAnimal
+    {
+        Female,
+        Masculine
+    }
+}
